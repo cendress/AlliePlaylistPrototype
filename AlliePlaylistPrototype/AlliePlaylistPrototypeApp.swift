f7122987fa0af4e41144abbac494e06ee22daa3d
cendress <95492688+cendress@users.noreply.github.com>
2026-05-01 15:13:15 -0400

    Initial Commit

---
+++ AlliePlaylistPrototype/AlliePlaylistPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlliePlaylistPrototypeApp.swift
+//  AlliePlaylistPrototype
+//
+//  Created by Christopher Endress on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct AlliePlaylistPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
